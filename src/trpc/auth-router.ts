4b525efcf57e8df5088cb8754989483512508982
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-02 14:27:09 +0530

    implemented verification email completely

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { getPayloadClient } from "../get-payload";
 import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validator";
 import { TRPCError } from "@trpc/server";
 import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
 
 export const authRouter = router({
   createPaylodUser: publicProcedure
@@ -28,5 +29,23 @@ export const authRouter = router({
       console.log("created...");
 
       return { success: true, sentToEmail: email };
+    }),
+
+  // creating verifyemail api endpoint.
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+      const payload = await getPayloadClient();
+
+      const isVerified = await payload.verifyEmail({
+        collection: "users",
+        token,
+      });
+      if (!isVerified) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+      return { success: true };
     }),
 });
