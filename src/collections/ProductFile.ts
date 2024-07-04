b8429823910a1cdc455a8bb129d492bc75c388e4
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-04 15:51:34 +0530

    completed data modelling

---
+++ src/collections/ProductFile.ts
@@ -0,0 +1,91 @@
+import { User } from "payload/dist/auth";
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import { Access, CollectionConfig } from "payload/types";
+
+const addUser: BeforeChangeHook = ({ req, data }) => {
+  const user = req.user as User | null;
+  return { ...data, user: user?.id };
+};
+
+const yourOwnAndPurchased: Access = async ({ req }) => {
+  const user = req.user as User | null;
+
+  if (user?.role === "admin") return true;
+  if (!user) return false;
+
+  const { docs: products } = await req.payload.find({
+    collection: "products",
+    depth: 0,
+    where: {
+      user: {
+        equals: user.id,
+      },
+    },
+  });
+
+  const ownProductFields = products.map((prod) => prod.product_files).flat();
+
+  const { docs: orders } = await req.payload.find({
+    collection: "orders",
+    depth: 2,
+    where: {
+      users: {
+        equals: user.id,
+      },
+    },
+  });
+
+  const purchasedProductFields = orders
+    .map((order) => {
+      return order.products.map((product) => {
+        if (typeof product === "string")
+          return req.payload.logger.error(
+            "search depth not sufficient to find purchased file IDs"
+          );
+
+        return typeof product.product_files === "string"
+          ? product.product_files
+          : product.product_files.id;
+      });
+    })
+    .filter(Boolean)
+    .flat();
+
+  return {
+    id: {
+      in: [...ownProductFields, ...purchasedProductFields],
+    },
+  };
+};
+
+export const ProductFiles: CollectionConfig = {
+  slug: "product_files",
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+  },
+  hooks: {
+    beforeChange: [addUser],
+  },
+  access: {
+    read: yourOwnAndPurchased,
+    update: ({ req }) => req.user === "admin",
+    delete: ({ req }) => req.user === "admin",
+  },
+  upload: {
+    staticURL: "/product_files",
+    staticDir: "product_files",
+    mimeTypes: ["image/*", "font/*", "application/postscript"],
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      admin: {
+        condition: () => false,
+      },
+      hasMany: false,
+      required: true,
+    },
+  ],
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,12 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 import path from "path";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { Users } from "./collections/Users";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
