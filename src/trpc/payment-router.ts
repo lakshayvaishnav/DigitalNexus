3f0f68cac08481c351948ae11c2f3603398d5480
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-07 00:14:54 +0530

    added privateprocedure secured routes

---
+++ src/trpc/payment-router.ts
@@ -0,0 +1,28 @@
+import { payload } from "payload";
+import { TRPCError } from "@trpc/server";
+import { privateProcedure, router } from "./trpc";
+import { z } from "zod";
+import { getPayloadClient } from "@/get-payload";
+export const paymentRouter = router({
+  createSession: privateProcedure
+    .input(z.object({ productIds: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx;
+      let { productIds } = input;
+
+      if (productIds.length === 0) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
+
+      const payload = await getPayloadClient();
+
+      const { docs: products } = await payload.find({
+        collection: "products",
+        where: {
+          id: {
+            in: productIds,
+          },
+        },
+      });
+    }),
+});
--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,9 +1,31 @@
 import { ExpressContext } from "@/server";
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { User } from "payload/dist/auth";
+import { PayloadRequest } from "payload/types";
 
 const t = initTRPC.context<ExpressContext>().create();
+const middleware = t.middleware;
+
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest;
+
+  const { user } = req as { user: User | null };
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx: {
+      user,
+    },
+  });
+});
 
 export const router = t.router;
 
 // public procedure these routes can be called by anybody...
 export const publicProcedure = t.procedure;
+
+// private procedure routes can be called by signed in user only.
+export const privateProcedure = t.procedure.use(isAuth);
