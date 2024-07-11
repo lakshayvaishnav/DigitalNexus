7ee18c56999880745e341f183913c6f95ae26627
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-11 11:51:53 +0530

    completed sign-up page and verfication email

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 import { ZodError, z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { trpc } from "@/trpc/client";
 import { toast } from "sonner";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { Label } from "@radix-ui/react-label";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
 
 const Page = () => {
   const router = useRouter();
@@ -44,6 +52,62 @@ const Page = () => {
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
     mutate({ email, password });
   };
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            {/* todo add logo here */}
+            <h1 className="text-2xl font-bold">Create An Account</h1>
+
+            <Link
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+              href="/sign-in"
+            >
+              Already have an account ? Sign-In
+              <ArrowRight />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register("email")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.email,
+                    })}
+                    placeholder="you@example.com"
+                  ></Input>
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input {...register("password")}></Input>
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <Button>Sign Up</Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Page;
