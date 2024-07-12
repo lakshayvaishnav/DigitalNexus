c604b69cfcbfcf890cc9e4c6ad8fff86aa3eca78
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-13 00:42:43 +0530

    added verify-email page

---
+++ src/app/(auth)/verify-email/page.tsx
@@ -0,0 +1,20 @@
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const VerifyEmailPage = ({ searchParams }: PageProps) => {
+  const token = searchParams.token;
+  const toEmail = searchParams.to;
+
+  return (
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        {token && typeof token === "string" ? (
+          <div className="grid gap-6"></div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
